2b681bfcc693f4a1ff4af93850002a37ee609c19
aarrunyal <aarrunyal@gmail.com>
2024-03-19 14:16:53 -0400

    working on email template

--- src/main/java/com/donationmanagementsystem/controller/ApiTestController.java
+++ src/main/java/com/donationmanagementsystem/controller/ApiTestController.java
@@ -15,7 +15,7 @@ import lombok.Data;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping(value="/api/test")
--- src/main/java/com/donationmanagementsystem/controller/TestController.java
+++ src/main/java/com/donationmanagementsystem/controller/TestController.java
@@ -2,22 +2,28 @@ package com.donationmanagementsystem.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping(value = "/test")
 public class TestController {
 
+    @GetMapping(value = "/email-template")
+    public String emailTemplate(Model model) {
+        model.addAttribute("message", "Hello from Thymeleaf!");
+        model.addAttribute("name", "Arun");
+        model.addAttribute("link", "Arun");
+        return "email/new-account";
+    }
+
     @GetMapping("/invoice")
     public String invoiceDesign() {
         return "redirect:invoice.html";
-    }
+    }   
 
-    @GetMapping(value="/check")
-    public String check(){
+    @GetMapping(value = "/check")
+    public String check() {
         return "Hey Hi, I am fine";
     }
 }
