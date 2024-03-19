671239318c6bd42d6c6a6596c39a84da5cdd4cf9
akhilesh k saju <akhilesh.saju@gmail.com>
2024-03-19 18:26:59 -0400

    changes

--- src/main/java/com/donationmanagementsystem/controller/ReportController.java
+++ src/main/java/com/donationmanagementsystem/controller/ReportController.java
@@ -1,17 +1,27 @@
 package com.donationmanagementsystem.controller;
 
-import org.springframework.http.ResponseEntity;
 
+
+import com.donationmanagementsystem.entity.User;
+import com.donationmanagementsystem.payload.request.DonationRequest;
+import com.donationmanagementsystem.payload.response.DonationResponse;
 import com.donationmanagementsystem.payload.response.ReportResponse;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+// @RequestMapping(value="/api/test/Report")
+// public class ReportController {
 
+//   @PostMapping
+//     public Respon seEntity<DonationResponse> post(
+//         @Valid @RequestBody DonationRequest donationRequest) {
+//         User user = userService.getLoggedInUser();
+//         return donationService.create(donationRequest, user);
+//     }
 
-public class ReportController {
+  
 
-  @GetMapping(value="/report")
-  public ResponseEntity<ReportResponse> reposEntityBuilder(){
-    return null;
-
-  }
-
-}
+// }
