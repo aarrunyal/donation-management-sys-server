306da0d986a813d1b62db8dc04439516298790e3
aarrunyal <aarrunyal@gmail.com>
2024-03-21 09:12:49 -0400

    conflict resolved

--- src/main/java/com/donationmanagementsystem/service/ReportService.java
+++ src/main/java/com/donationmanagementsystem/service/ReportService.java
@@ -1,12 +1,11 @@
 package com.donationmanagementsystem.service;
 
-import com.donationmanagementsystem.entity.ReportDTO;
 import java.util.List;
 
 
 public interface ReportService {
-  ReportDTO saveReport(ReportDTO reportDTO);
-    ReportDTO getReportById(int id);
-    List<ReportDTO> getAllReports();
-    void deleteReportById(int id);
+  // ReportDTO saveReport(ReportDTO reportDTO);
+  //   ReportDTO getReportById(int id);
+  //   List<ReportDTO> getAllReports();
+  //   void deleteReportById(int id);
 }
